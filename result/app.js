344870db72287fa602336a1f1f97b8b283c3834b
Diego Garcia Gonzalez <diego.garcia2084@alumnos.udg.mx>
2022-05-22 00:52:31 -0500

    result app con ejs

--- result/app.js
+++ result/app.js
@@ -1,89 +1,76 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-
-const fs = require('fs');
-
+const { timeEnd } = require('console')
+const express = require('express')
+const app = express()
+const path = require('path')
 const { Client } = require('pg')
 
-app.use(express.static(path.join(__dirname, 'stylesheets')));
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  
-  //Conexion DB
-  const connectionData = {
-    user: 'Diana',
-    host: 'postgresql.diana2206-dev.svc.cluster.local',
-    database: 'votingApp',
-    password: '220600dfcr',
-    port: 5432,
-  }
-
-  const client = new Client(connectionData);
-  client.connect();
-
-  client.query("SELECT vote, COUNT(voter_id) AS count FROM votos GROUP BY vote", 
-    function(err,result){
-    if(err) {
-        throw err;
-    }
-    else {
-      var votes = collectVotesFromResult(result);
-      //Modificar HTML
-      html = fs.readFileSync(__dirname + '/index.html').toString()
-      id_dom = new JSDOM(html)
-
-      total = votes.a + votes.b;
-      porcentaje_a = (votes.a*100)/total;
-      porcentaje_b = (votes.b*100)/total;
-      id_dom.window.document.getElementById("catstats").innerHTML = porcentaje_a.toFixed(2) + "%";
-      id_dom.window.document.getElementById("dogstats").innerHTML = porcentaje_b.toFixed(2) + "%";
-
-      div1 = id_dom.window.document.getElementById('background-stats-1');
-      div2 = id_dom.window.document.getElementById('background-stats-2');
-      
-      if(total > 0){
-        div1.style.width = (votes.a*100)/total + "%"
-        div2.style.width = (votes.b*100)/total + "%"
+    const connectionData = {
+        user: 'Diana',
+        host: 'postgresql.diana2206-dev.svc.cluster.local',
+        database: 'votingApp',
+        password: '220600dfcr',
+        port: 5432,
       }
-      else{
-        div1.style.width = "50%"
-        div2.style.width = "50%"
-      }
-      
+    
+    const client = new Client(connectionData);
+    client.connect();
+    
+    client.query("SELECT vote, COUNT(voter_id) AS count FROM votos GROUP BY vote", 
+        function(err,result){
+            if(err){
+                throw err;
+            }
+            else{
+                var votes = collectVotesFromResult(result);
+                var total = votes.a + votes.b;
+                var cats_percent, dogs_percent, div_izq, div_der;
+        
+                if(total == 0){
+                    cats_percent = 0;
+                    dogs_percent = 0;
+                    div_izq = 50;
+                    div_der = 50;
+                }
+                else{
+                    cats_percent = (votes.a*100)/total;
+                    dogs_percent = (votes.b*100)/total;
+                    div_izq = cats_percent;
+                    div_der = dogs_percent;
+                }
 
-      t_votos= id_dom.window.document.getElementById('total')
-      if(total == 0)
-        t_votos.innerHTML = "0 votos";
-      
-      if(total == 1)
-        t_votos.innerHTML = "1 voto";
-
-      if(total > 1)
-      t_votos.innerHTML = total + " votos";
-
-      fs.writeFileSync(__dirname + '/index.html', id_dom.serialize());
-    }
-    })
+                if(total == 1)
+                    var total_string = total + " voto"
+                else
+                    var total_string = total + " votos"
+                
+                res.render("index", 
+                {
+                    total: total_string,
+                    cats_percent: cats_percent.toFixed(2),
+                    dogs_percent: dogs_percent.toFixed(2),
+                    div_izq: div_izq.toFixed(2),
+                    div_der: div_der.toFixed(2)
+                });
+            }
+        }
+        
+        )
 
     function collectVotesFromResult(result) {
         var votes = {a: 0, b: 0};
-      
+          
         result.rows.forEach(function (row) {
-          votes[row.vote] = parseInt(row.count);
+            votes[row.vote] = parseInt(row.count);
         });
-      
+          
         return votes;
     }
-
-
-  res.sendFile(path.join(__dirname + '/index.html'));
-
+  
 })
-
 
 app.listen(3000)
