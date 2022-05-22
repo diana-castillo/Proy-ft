dee64d6f8f43419c6dff7d327103767f4b89bfbe
Diana Fernanda Castillo Rebolledo <diana.castillo7676@alumnos.udg.mx>
2022-05-22 16:30:36 -0500

    Enlace y puerto en python v2

--- python_v2/app.py
+++ python_v2/app.py
@@ -64,6 +64,6 @@ def hola(id = None):
     
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=8001, debug=True, threaded=True)
 
 conexion.close()
